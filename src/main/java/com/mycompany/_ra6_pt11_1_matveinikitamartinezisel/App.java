7e13cbb6a87b2e2b20eee675130f897060b96f6e
nikita <nikitamatpech@gmail.com>
2025-03-18 13:30:47 +0100

    Versió incial

---
+++ src/main/java/com/mycompany/_ra6_pt11_1_matveinikitamartinezisel/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._ra6_pt11_1_matveinikitamartinezisel;
+
+/**
+ *
+ * @author nikit
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
